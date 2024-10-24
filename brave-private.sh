c0ec3f94835b42c1a7c03d6e93eb1eb3520ce315
gandres42 <gandres2416@gmail.com>
2024-10-24 11:21:39 -0700

    remove dead scripts, add plsama icon updater

--- brave-private.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-filename="Brave-$(date +%s%N)"
-cp -r /home/gavin/.config/BraveSoftware/Brave-Browser /tmp/$filename
-rm /tmp/$filename/Singleton*
-/usr/bin/brave-browser-stable --class=Private --user-data-dir=/tmp/$filename --incognito
-rm -rf /tmp/$filename

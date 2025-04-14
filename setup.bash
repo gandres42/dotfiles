91a8e8f52d9abdf1d2562390588231f64a275eab
gandres42 <gandres2416@gmail.com>
2025-04-14 13:27:52 -0700

    raaah i have package managers

--- setup.bash
+++ setup.bash
@@ -34,27 +34,3 @@ if [[ -n "$CONTAINER_ID" ]]; then
         ;;
     esac
 fi
-
-# MAMBA
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/gavin/.miniforge/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home/gavin/.miniforge/etc/profile.d/conda.sh" ]; then
-        . "/home/gavin/.miniforge/etc/profile.d/conda.sh"
-    else
-        export PATH="/home/gavin/.miniforge/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-
-if [ -f "/home/gavin/.miniforge/etc/profile.d/mamba.sh" ]; then
-    . "/home/gavin/.miniforge/etc/profile.d/mamba.sh"
-fi
-# <<< conda initialize <<<
-
-if [ -n "$CONDA_PREFIX" ]; then
-    source "${CONDA_PREFIX}/setup.bash" >/dev/null 2>&1
-fi

95b839e27162e3dd5e399776ee79dedd7b9e1779
gandres42 <gandres2416@gmail.com>
2025-04-13 22:55:17 -0700

    nvm, pixi compatibity not there yet

--- setup.bash
+++ setup.bash
@@ -33,10 +33,22 @@ if [ -d $HOME/.dotfiles/functions ]; then
     done
 fi
 
-#PIXI
-export PATH="/home/gavin/.pixi/bin:$PATH"
+# MAMBA
+if [[ "$TERM_PROGRAM" != "vscode" ]]; then
+    # !! Contents within this block are managed by 'conda init' !!
+    __conda_setup="$('/home/gavin/.miniforge/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "/home/gavin/.miniforge/etc/profile.d/conda.sh" ]; then
+            . "/home/gavin/.miniforge/etc/profile.d/conda.sh"
+        else
+            export PATH="/home/gavin/.miniforge/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
 
-if [[ "$TERM_PROGRAM" == "vscode" ]]; then
-    eval "$(pixi shell-hook)"
-    clear
+    if [ -f "/home/gavin/.miniforge/etc/profile.d/mamba.sh" ]; then
+        . "/home/gavin/.miniforge/etc/profile.d/mamba.sh"
+    fi
 fi

425faf1ebd6236b736f405cf18c83e57dcfba122
gandres42 <gandres2416@gmail.com>
2025-04-15 12:37:42 -0700

    clean functions, reformat setup

--- setup.bash
+++ setup.bash
@@ -1,19 +1,44 @@
-# ALIASES
+# ALIASES ---------------------------------------------------------------------
 alias wake-keats="ssh discovision \"wakeonlan D8:5E:D3:D9:EF:E4\""
 alias wp-keats="waypipe -c lz4=8 --video=hw ssh -Y -C keats"
 alias ros-conda="source "${CONDA_PREFIX}/setup.bash" >/dev/null 2>&1"
 
-# SHELL SCRIPTS
+# SHELL SCRIPTS ---------------------------------------------------------------
 export PATH="$HOME/.dotfiles/scripts:$PATH"
 
-# FUNCTIONS
-if [ -d $HOME/.dotfiles/functions ]; then
-    for file in $HOME/.dotfiles/functions/*; do
-        [ -f "$file" ] && source "$file"
-    done
-fi
+# FUNCTIONS -------------------------------------------------------------------
+db() {
+    if [ "$1" == "code" ]; then
+        distrobox-code "$2"
+    elif [ "$1" == "uncode" ]; then
+        distrobox-uncode "$2"
+    elif [ "$1" == "startall" ]; then
+        distrobox-startall
+    elif [ "$1" == "create" ]; then
+        distrobox "${@:1}"
+        distrobox-codeall
+    else
+        distrobox "${@:1}"
+    fi
+}
 
-# DISTROBOX
+ts() {
+    if [ "$1" == "mull" ]; then
+        tailscale set --exit-node=$(tailscale exit-node suggest | awk -F': ' '/Suggested exit node:/ {print $2}' | sed 's/\.$//')
+        ipcheck
+    elif [ "$1" == "unmull" ]; then
+        tailscale set --exit-node=
+        ipcheck
+    else
+        tailscale "${@:1}"
+    fi
+}
+
+ros-pixi() {
+    source "${CONDA_PREFIX}/setup.bash" >/dev/null 2>&1
+}
+
+# DISTROBOX -------------------------------------------------------------------
 if [[ -n "$CONTAINER_ID" ]]; then
     PS1="📦[\u@${CONTAINER_ID} \W]\$ "
     case $CONTAINER_ID in
@@ -35,5 +60,5 @@ if [[ -n "$CONTAINER_ID" ]]; then
     esac
 fi
 
-# PIXI
+# PIXI ----------------------------------------------------------------------------------
 export PATH="/home/gavin/.pixi/bin:$PATH"

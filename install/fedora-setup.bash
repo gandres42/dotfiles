7a6a404985b8d4e44b7331bf57d4efb1e0bc8cd6
gandres42 <gandres2416@gmail.com>
2025-05-12 19:48:40 -0700

    fix file sync, add bash extensions

--- install/fedora-setup.bash
+++ install/fedora-setup.bash
@@ -1,1 +1,3 @@
-sudo dnf remove akregator dragon elisa-player libreoffice* kaddressbook kamoso kcalc kde-connect kmahjongg kmines kmouth kolourpaint kpat kontact korganizer krdc krfb neochat skanpage
+sudo dnf remove -y akregator dragon elisa-player libreoffice* kaddressbook kamoso kcalc kde-connect kmahjongg kmines kmouth kolourpaint kpat kontact korganizer krdc krfb neochat skanpage
+sudo dnf install -y qt
+

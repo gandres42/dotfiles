de9b6937c02bf14169a7027970a871f780bb9643
gandres42 <gandres2416@gmail.com>
2025-07-30 16:32:54 -0700

    enable uv bytecode compilation by default

--- setup.bash
+++ setup.bash
@@ -11,77 +11,36 @@ alias re-source="source ~/.bashrc"
 alias get-mit="wget https://www.mit.edu/~amini/LICENSE.md"
 alias ipcheck="curl -s http://ip-api.com/json/ | jq"
 
-# VSCODE AUTO-ACTIVATION ------------------------------------------------------
-if [[ "$TERM_PROGRAM" == "vscode" ]]; then
-    dir="$PWD"
-    while [[ "$dir" != "/" ]]; do
-        if [[ -d "$dir/.pixi" || -d "$dir/.venv" || -e "$dir/.condaenv" ]]; then
-            break
-        fi
-        dir="$(dirname "$dir")"
-    done
-    export VSCODE_WORKSPACE_ROOT="$dir"
-    if [ -d $dir/.pixi ]; then
-        eval "$($HOME/.pixi/bin/pixi shell-hook)"
-        if [ -f $PIXI_PROJECT_ROOT/.pixi/envs/default/setup.bash ]; then
-            source $PIXI_PROJECT_ROOT/.pixi/envs/default/setup.bash
-        fi
-    elif [ -d $dir/.venv ]; then
-        source $VSCODE_WORKSPACE_ROOT/.venv/bin/activate
-    elif [ -e $dir/.condaenv ]; then
-        mamba activate $(cat .condaenv)
-    fi
-    clear
-fi
-
-# PIXI ------------------------------------------------------------------------
-if [[ -e "$HOME/.pixi" ]]; then
-    export PATH="/home/gavin/.pixi/bin:$PATH"
-    pixi() {
-        if [ "$1" == "shell" ]; then
-            bash --rcfile <(echo 'eval "$(pixi shell-hook --change-ps1 false)"'; cat ~/.bashrc; echo '[ -f "$PIXI_PROJECT_ROOT/.pixi/envs/default/setup.bash" ] && source "$PIXI_PROJECT_ROOT/.pixi/envs/default/setup.bash"'; echo '[[ -z "$PIXI_PROJECT_ROOT" ]] && exit 1;'; echo 'export PS1="($PIXI_PROJECT_NAME) $PS1"')
-        elif [ "$1" == "pip" ]; then
-            if [ -n "$PIXI_PROJECT_NAME" ]; then
-                $HOME/.local/bin/uv "${@:1}" --system
-            else
-                echo -e "Error:   \e[31m×\e[0m could not find pixi.toml or pyproject.toml at directory $PWD"
-            fi
-        else
-            $HOME/.pixi/bin/pixi "${@:1}"
-        fi
-    }
-fi
-
 # MAMBA -----------------------------------------------------------------------
-if [[ -e "$HOME/.miniforge" ]]; then
-    # >>> conda initialize >>>
-    # !! Contents within this block are managed by 'conda init' !!
-    __conda_setup="$('/home/gavin/.miniforge/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-    if [ $? -eq 0 ]; then
-        eval "$__conda_setup"
-    else
-        if [ -f "/home/gavin/.miniforge/etc/profile.d/conda.sh" ]; then
-            . "/home/gavin/.miniforge/etc/profile.d/conda.sh"
-        else
-            export PATH="/home/gavin/.miniforge/bin:$PATH"
-        fi
-    fi
-    unset __conda_setup
-    # <<< conda initialize <<<
-
-    # >>> mamba initialize >>>
-    # !! Contents within this block are managed by 'mamba shell init' !!
-    export MAMBA_EXE='/home/gavin/.miniforge/bin/mamba';
-    export MAMBA_ROOT_PREFIX='/home/gavin/.miniforge';
-    __mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
-    if [ $? -eq 0 ]; then
-        eval "$__mamba_setup"
-    else
-        alias mamba="$MAMBA_EXE"  # Fallback on help from mamba activate
-    fi
-    unset __mamba_setup
-    # <<< mamba initialize <<<
-fi
+# if [[ -e "$HOME/.miniforge" ]]; then
+#     # >>> conda initialize >>>
+#     # !! Contents within this block are managed by 'conda init' !!
+#     __conda_setup="$('/home/gavin/.miniforge/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+#     if [ $? -eq 0 ]; then
+#         eval "$__conda_setup"
+#     else
+#         if [ -f "/home/gavin/.miniforge/etc/profile.d/conda.sh" ]; then
+#             . "/home/gavin/.miniforge/etc/profile.d/conda.sh"
+#         else
+#             export PATH="/home/gavin/.miniforge/bin:$PATH"
+#         fi
+#     fi
+#     unset __conda_setup
+#     # <<< conda initialize <<<
+#
+#     # >>> mamba initialize >>>
+#     # !! Contents within this block are managed by 'mamba shell init' !!
+#     export MAMBA_EXE='/home/gavin/.miniforge/bin/mamba';
+#     export MAMBA_ROOT_PREFIX='/home/gavin/.miniforge';
+#     __mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+#     if [ $? -eq 0 ]; then
+#         eval "$__mamba_setup"
+#     else
+#         alias mamba="$MAMBA_EXE"  # Fallback on help from mamba activate
+#     fi
+#     unset __mamba_setup
+#     # <<< mamba initialize <<<
+# fi
 
 # DISTROBOX -------------------------------------------------------------------
 if [[ -n "$CONTAINER_ID" ]]; then
@@ -109,6 +68,48 @@ db() {
     fi
 }
 
+# VSCODE AUTO-ACTIVATION ------------------------------------------------------
+if [[ "$TERM_PROGRAM" == "vscode" ]]; then
+    dir="$PWD"
+    while [[ "$dir" != "/" ]]; do
+        if [[ -d "$dir/.pixi" || -d "$dir/.venv" || -e "$dir/.condaenv" ]]; then
+            break
+        fi
+        dir="$(dirname "$dir")"
+    done
+
+    export VSCODE_WORKSPACE_ROOT="$dir"
+    if [ -d $dir/.pixi ]; then
+        eval "$($HOME/.pixi/bin/pixi shell-hook)"
+        if [ -f $PIXI_PROJECT_ROOT/.pixi/envs/default/setup.bash ]; then
+            source $PIXI_PROJECT_ROOT/.pixi/envs/default/setup.bash
+        fi
+    elif [ -d $dir/.venv ]; then
+        source $VSCODE_WORKSPACE_ROOT/.venv/bin/activate
+    elif [ -e $dir/.condaenv ]; then
+        mamba activate $(cat .condaenv)
+    fi
+    clear
+fi
+
+# PIXI ------------------------------------------------------------------------
+if [[ -e "$HOME/.pixi" ]]; then
+    export PATH="/home/gavin/.pixi/bin:$PATH"
+    pixi() {
+        if [ "$1" == "shell" ]; then
+            bash --rcfile <(echo 'eval "$(pixi shell-hook --change-ps1 false)"'; cat ~/.bashrc; echo '[ -f "$PIXI_PROJECT_ROOT/.pixi/envs/default/setup.bash" ] && source "$PIXI_PROJECT_ROOT/.pixi/envs/default/setup.bash"'; echo '[[ -z "$PIXI_PROJECT_ROOT" ]] && exit 1;'; echo 'export PS1="($PIXI_PROJECT_NAME) $PS1"')
+        elif [ "$1" == "pip" ]; then
+            if [ -n "$PIXI_PROJECT_NAME" ]; then
+                $HOME/.local/bin/uv "${@:1}" --system --compile
+            else
+                echo -e "Error:   \e[31m×\e[0m could not find pixi.toml or pyproject.toml at directory $PWD"
+            fi
+        else
+            $HOME/.pixi/bin/pixi "${@:1}"
+        fi
+    }
+fi
+
 # TAILSCALE  ------------------------------------------------------------------
 ts() {
     if [ "$1" == "mull" ]; then
@@ -127,11 +128,12 @@ if [[ "$ROS_DISTRO" == "noetic" ]]; then
     alias cbs="catkin build && source devel/setup.bash"
     alias s="source devel/setup.bash"
     alias plotjuggler="rosrun plotjuggler plotjuggler -n"
+    export DISABLE_ROS1_EOL_WARNINGS=1
 elif [[ -n "$ROS_DISTRO" ]]; then
     alias cbs="colcon build && source install/setup.bash"
     alias s="source install/setup.bash"
     alias plotjuggler="ros2 run plotjuggler plotjuggler -n"
-    alias foxglove-bridge="ros2 launch foxglove_bridge foxglove_bridge_launch.xml" # use_compression:=true"
+    alias foxglove-bridge="ros2 launch foxglove_bridge foxglove_bridge_launch.xml use_compression:=true"
     alias roscore="ros2 run rmw_zenoh_cpp rmw_zenohd"
     export COLCON_EXTENSION_BLOCKLIST=colcon_core.event_handler.desktop_notification
     export RMW_IMPLEMENTATION=rmw_zenoh_cpp
